9e752a35153bafcb5e2f74971963c71945df625b
zhaoran802 <158826051+zhaoran802@users.noreply.github.com>
2025-12-06 15:10:51 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
 	//网页元数据
 	metaData: {
-		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
-		keywords: 'Leleo,leleo,个人主页,个人网站',
+		title: 'Marisa X的个人主页🎉',
+		description: '欢迎来到Marisa X的奇妙世界！',
+		keywords: 'Marisa X,Marisa X,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
 
 	avatar: "/img/avatar.jpg", // 头像
-	welcometitle: "Hi, I'm Leleo", // 标题
+	welcometitle: "Hi, I'm Marisa X", // 标题
 
 	// 颜色配置
 	color: {
